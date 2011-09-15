623525e6a319096cec960be12b3acb4356a11d03
Simen Svale Skogsrud <simen@bengler.no>
2011-09-15 21:49:37 +0200

    renamed class

--- lib/future.rb
+++ lib/future.rb
@@ -1,43 +1,45 @@
-class FutureValue
-  class ValueNeverArrived < Exception
-  end
+module Future
+  class Value
+    class ValueNeverArrived < Exception
+    end
 
-  def initialize(&block)
-    @thread = Thread.new(&block)
-  end
+    def initialize(&block)
+      @thread = Thread.new(&block)
+    end
 
-  def value
-    @thread.value
-  end
+    def value
+      @thread.value
+    end
 
-  def done?
-    !@thread.alive?
-  end
+    def done?
+      !@thread.alive?
+    end
 
-  def error?
-    @thread.status == nil
-  end
+    def error?
+      @thread.status == nil
+    end
 
-  def method_missing(method, *args, &block)      
-    self.value.send(method, *args, &block) 
-  end
+    def method_missing(method, *args, &block)      
+      self.value.send(method, *args, &block) 
+    end
 
-  def to_s
-    self.value.to_s
-  end
-  
-  def to_str
-    self.value.to_str
-  end
+    def to_s
+      self.value.to_s
+    end
+    
+    def to_str
+      self.value.to_str
+    end
 
-  def inspect 
-    if @thread.alive?
-      "#<#{self.class.name} value=[[pending]]>"
-    else
-      begin
-        "#<#{self.class.name} value=#{self.value.inspect}>"
-      rescue
-        "#<#{self.class.name} value=[[failed]]>"
+    def inspect 
+      if @thread.alive?
+        "#<#{self.class.name} value=[[pending]]>"
+      else
+        begin
+          "#<#{self.class.name} value=#{self.value.inspect}>"
+        rescue
+          "#<#{self.class.name} value=[[failed]]>"
+        end
       end
     end
   end
